58347c1ccef8f9126692d1ad49aedb2f0f4093b2
Daniel <Danielsmidstrup1@gmail.com>
2023-04-07 10:03:34 +0200

    Create functionality added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import UserRow from "./Userrow";
+import UserRow from "./components/UserRow";
+import NewUser from "./components/NewUser";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [newUsers, setNewUsers] = useState([]);
+  const [newUserMaxID, setnewUserMaxID] = useState(0);
 
-  useEffect((users) => {
+  useEffect(() => {
     fetch("http://127.0.0.1:8080/users")
       .then((response) => response.json())
       .then((data) => {
@@ -24,6 +27,21 @@ function App() {
     });
   }
 
+  function addNewUserInput() {
+    setNewUsers((newUsers) => [...newUsers, newUserMaxID]);
+    setnewUserMaxID((maxID) => newUserMaxID + 1);
+  }
+
+  function deleteNewUser(id) {
+    setNewUsers((oldValues) => {
+      return oldValues.filter((user) => user !== id);
+    });
+  }
+
+  function updateNewUser(newUser) {
+    setUsers([...users, newUser]);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -31,10 +49,22 @@ function App() {
       </header>
       <div>
         {users.map((user) => (
-          <div key={user.id} className="User">
+          <div key={user.id}>
             <UserRow user={user} deleteUser={deleteUser} />
           </div>
         ))}
+      </div>
+      {newUsers.map((newUserID) => (
+        <div key={newUserID}>
+          <NewUser
+            id={newUserID}
+            deleteUser={deleteNewUser}
+            updateNewUser={updateNewUser}
+          ></NewUser>
+        </div>
+      ))}
+      <div>
+        <button onClick={() => addNewUserInput()}>New User</button>
       </div>
     </div>
   );
---
+++ frontend/src/components/NewUser.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { useState } from "react";
+import "../App.css";
+
+export default function UserRow(probs) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [status, setStatus] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    let newUser = { name: name, email: email, status: status };
+
+    fetch("http://127.0.0.1:8080/create-user", {
+      method: "POST",
+      body: JSON.stringify(newUser),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        probs.deleteUser(probs.id);
+        probs.updateNewUser({
+          id: data,
+          name: name,
+          email: email,
+          status: status,
+        });
+      })
+      .catch(() => {
+        console.log("Somthing failed");
+      });
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div className="NewUser">
+        <div className="InputBox">
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <div className="InputBox">
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </div>
+        <div className="InputBox">
+          <label>Status:</label>
+          <input
+            type="text"
+            name="status"
+            onChange={(event) => setStatus(event.target.value)}
+          />
+        </div>
+        <button type="submit">Submit</button>
+        <button onClick={() => probs.deleteUser(probs.id)}>Delete</button>
+      </div>
+    </form>
+  );
+}
